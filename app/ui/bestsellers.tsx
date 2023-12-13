ef8a9ce982205ea1306d9d8cce6a34dd4d21b868
Michal Jurczuk <michal.jurczuk91@gmail.com>
2023-12-13 15:40:23 +0100

    fixed temporary bastsellers list to comply with new product definition

--- app/ui/bestsellers.tsx
+++ app/ui/bestsellers.tsx
@@ -1,44 +1,52 @@
 import ProductMiniature from "./product-miniature"
-import { Product } from "../lib/definitions"
+import { Product } from "../(lib)/definitions"
 
 export default function Bestsellers() {
     const products: Product[] = [
         {
-            id: 0,
             name: 'guitar',
-            description_short: 'jaka jest każdy widzi',
+            category: 'guitars',
+            subcategory: 'acoustic guitars',
+            amount: 10,
+            description: 'jaka jest każdy widzi',
             image_url: '/guitar.jpg',
             price: 500.99,
         },
         {
-            id: 1,
             name: 'guitar',
-            description_short: 'jaka jest każdy widzi',
+            category: 'guitars',
+            subcategory: 'acoustic guitars',
+            amount: 10,
+            description: 'jaka jest każdy widzi',
             image_url: '/guitar.jpg',
-            price: 666.99,
+            price: 500.99,
         },
         {
-            id: 2,
             name: 'guitar',
-            description_short: 'jaka jest każdy widzi',
+            category: 'guitars',
+            subcategory: 'acoustic guitars',
+            amount: 10,
+            description: 'jaka jest każdy widzi',
             image_url: '/guitar.jpg',
-            price: 333.99,
+            price: 500.99,
         },
         {
-            id: 3,
             name: 'guitar',
-            description_short: 'jaka jest każdy widzi',
+            category: 'guitars',
+            subcategory: 'acoustic guitars',
+            amount: 10,
+            description: 'jaka jest każdy widzi',
             image_url: '/guitar.jpg',
-            price: 222.99,
-        }
+            price: 500.99,
+        },
     ]
     return <div className="w-full mt-4 flex justify-center">
         <div className="max-w-6xl">
             <h2 className=" text-2xl uppercase font-bold text-center">Bestsellers</h2>
 
             <div className="w-full flex flex-col items-center md:grid md:grid-cols-2 md:grid-rows-2 md:gap-4 lg:grid-cols-4 lg:grid-rows-1 lg:gap-4">
-                {products.map(product => {
-                    return <ProductMiniature key={product.id} {...product} />
+                {products.map((product, index) => {
+                    return <ProductMiniature key={index} {...product} />
                 })}
             </div>
         </div>
