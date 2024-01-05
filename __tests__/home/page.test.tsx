fbc89e1b549e1b06a5e7dc9973a473d5fa748ea1
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-01-05 12:22:45 +0100

    jest runs tests

---
+++ __tests__/home/page.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react"
+import Home from "@/app/page"
+
+describe('Homepage', () => {
+    it("runs tests", () => {
+        const one = 1;
+        expect(one).toEqual(1);
+    })
+})
