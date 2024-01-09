2a6adfff0a0e341964d4767c9d2615b3584ee74c
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-01-09 13:49:53 +0100

    bestsellers tests

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -1,11 +1,48 @@
-import { expect, test, beforeAll, afterEach, afterAll } from 'vitest'
+import { expect, test, describe } from 'vitest'
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
+import userEvent from "@testing-library/user-event"
+import { render, screen } from '@testing-library/react'
 import Bestsellers from '../app/(ui)/bestsellers'
 import { products } from '../mocks/mockData.js'
- 
+import {server} from "../mocks/mswServer.js"
 
-test('Page', async () => {
-    const bestsellers = await Bestsellers();
-    render(bestsellers);
+
+/* test('bestsellers', async () => {
+
+    for(let p of products){
+        const item = screen.getByText(p.name);
+        expect(item).toBeInTheDocument();
+        const itemDescription = screen.getByText(p.description);
+        expect(itemDescription).toBeInTheDocument();
+    }
+}) */
+
+describe('bestsellers', () => {
+    const renderBestsellers = async () => {
+        const bestsellers = await Bestsellers();
+        render(bestsellers);
+    }
+
+    test('bestsellers section displays product names', async () => {
+        await renderBestsellers();
+        for(let p of products){
+            const productName = screen.getByText(p.name);
+            expect(productName).toBeInTheDocument();
+        };
+    });
+    test('bestsellers section displays products descriptions', async () => {
+        await renderBestsellers();
+        for(let p of products){
+            const productDescription = screen.getByText(p.description);
+            expect(productDescription).toBeInTheDocument();
+        };
+    });
+    test('clicking on bestsellers product name redirects to that products page'), async () => {
+        const user = userEvent.setup()
+        await renderBestsellers();
+        const firstProductName = screen.getByText(products[0].name);
+        await user.click(firstProductName);
+        const product = screen.getByText('PRODUCT');
+        expect(product).toBeInTheDocument();
+    }
 })
--- vitest.setup.js
+++ vitest.setup.js
@@ -1,17 +1,17 @@
 import * as matchers from 'vitest-dom/matchers'
-import {expect} from 'vitest'
-import { afterEach } from 'vitest';
+import { afterAll, beforeAll, afterEach, expect } from 'vitest'
+import {  } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-import {server} from "./mocks/mswServer.js"
+import { server } from "./mocks/mswServer.js"
 
 expect.extend(matchers)
 
 beforeAll(() => server.listen())
 
 afterEach(() => {
-  cleanup();
-  server.resetHandlers();
+    cleanup();
+    server.resetHandlers();
 });
 
 afterAll(() => server.close())
