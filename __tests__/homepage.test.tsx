45b10cf86d653d0a2ae80ad0ebdb8035da9a2ab8
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-01-10 12:12:45 +0100

    product miniature tests

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -5,34 +5,44 @@ import { render, screen } from '@testing-library/react'
 import Bestsellers from '../app/(ui)/bestsellers'
 import BlogArticleStubs from '../app/(ui)/blogArticleStubs'
 import { products, blogArticles } from '../mocks/mockData.js'
+import ProductMiniature from '../app/(ui)/productMiniature'
 
-describe('bestsellers section', () => {
-    const renderBestsellers = async () => {
-        const bestsellers = await Bestsellers();
-        render(bestsellers);
-    }
-    it('displays product names', async () => {
-        await renderBestsellers();
-        for(let p of products){
-            const productName = screen.getByText(p.name);
-            expect(productName).toBeInTheDocument();
-        };
+describe('product miniature', () => {
+    test('displays products name', () => {
+        render(<ProductMiniature product={products[0]} />);
+        const name = screen.getByText(products[0].name);
+        expect(name).toBeInTheDocument();
     });
-    it('displays products descriptions', async () => {
-        await renderBestsellers();
-        for(let p of products){
-            const productDescription = screen.getByText(p.description);
-            expect(productDescription).toBeInTheDocument();
-        };
+    test('displays first 30 letters of product description', () => {
+        render(<ProductMiniature product={products[0]} />);
+        const description = screen.getByText(products[0].description.slice(0,29));
+        expect(description).toBeInTheDocument();
     });
-    test('clicking on bestsellers product name redirects to that products page'), async () => {
+    test('displays products image', () => {
+        render(<ProductMiniature product={products[0]} />);
+        const img = screen.getByAltText('product image') as HTMLImageElement;
+        expect(img.src).toContain(`http://localhost:3000${products[0].image_url}`);
+    })
+    test('clicking on  product name redirects to that products page'), async () => {
         const user = userEvent.setup()
-        await renderBestsellers();
-        const firstProductName = screen.getByText(products[0].name);
-        await user.click(firstProductName);
+        const name = screen.getByText(products[0].name);
+        await user.click(name);
         const product = screen.getByText('PRODUCT');
         expect(product).toBeInTheDocument();
     }
+})
+
+describe('bestsellers section', () => {
+    test('bestsellers section renders 4 product miniatures', async () => {
+        const bestsellers = await Bestsellers();
+        render(bestsellers);
+        for(let i = 0; i < 4; i++){
+            const name = screen.getByText(products[i].name);
+            const description = screen.getByText(products[i].description.slice(0,29));
+            expect(name).toBeInTheDocument();
+            expect(description).toBeInTheDocument();
+        }
+    })
 });
 
 describe('blog articles section', () => {
@@ -52,7 +62,6 @@ describe('blog articles section', () => {
         const images = screen.getAllByAltText("article image") as HTMLImageElement[];
         const imageSrcs = images.map(img=>img.src);
         console.log(imageSrcs);
-        // ?? nie podoba mi się
         for(let article of blogArticles){  
             expect(imageSrcs).toContain(`http://localhost:3000${article.image_url}`);
         }
--- app/(ui)/navbar/categoryMenuItem.tsx
+++ app/(ui)/navbar/categoryMenuItem.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link"
+import { Category } from "@/app/(lib)/definitions"
 
-export default function CategoryMenuItem({ name, subitems }: { name: string, subitems: string[] }) {
+type Props = {
+    category: Category,
+}
+
+export default function CategoryMenuItem({category:{ name, subcategories }}:Props) {
     return <div className="group">
         <Link className="mx-4" href={`category`}>
             <span className="capitalize">{name}</span>
         </Link>
         <div className="invisible absolute flex flex-col p-6 bg-white shadow-md group-hover:visible">
-            {subitems.map((item) => {
+            {subcategories.map((item) => {
                 return <Link key={item} href={`/category`}>
                     <span className="capitalize">{item}</span>
                 </Link>
