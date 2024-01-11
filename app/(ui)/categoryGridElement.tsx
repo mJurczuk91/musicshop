c38dd8cb615113514b67e77add29d5c9b376de7e
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-01-11 09:01:11 +0100

    category grid fixed

--- app/(ui)/categoryGridElement.tsx
+++ app/(ui)/categoryGridElement.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link"
 
 export default function CategoryGridElement({name, subcategories} : {name:string, subcategories:string[]}){
-    return <div style={{}} className="flex justify-between p-2 border-gray-400 border-opacity-25 odd:ml-4 even:mr-4 lg:mx-0 lg:first:border-r-0 lg:first:border-b-0 lg:last:border-t-0 lg:m-4 lg:p-4 lg:last:pr-0 lg:first:pl-0">
+    return <div style={{}} className="flex justify-between p-2 border-gray-400 border-opacity-25">
         <div className="flex flex-col">
-            <Link href={`/category/${name}`} className=" font-bold tracking-tight">{name}</Link>
+            <Link href={`/category/${name}`} className="capitalize font-bold tracking-tight">{name}</Link>
             {subcategories.map(item => {
-                item[0].toUpperCase();
-                return <Link key={item} className="tracking-tight" href={`/category/${name}/${item}`}>{item}</Link>
+                return <Link key={item} className="tracking-tight capitalize" href={`/category/${name}/${item}`}>{item}</Link>
             })}
         </div>
         <img src={`/svg/${name}.svg`} alt={name} className="hidden lg:block h-auto w-16" />
--- app/(ui)/navbar/categoryMenu.tsx
+++ app/(ui)/navbar/categoryMenu.tsx
@@ -3,6 +3,7 @@ import CategoryMenuItem from "./categoryMenuItem"
 
 export default async function CategoryMenu() {
     const categories = await fetchCategories();
+    
     return <div className="w-full flex justify-center">
         <div className=" max-w-6xl w-full p-4 flex justify-center">
             {categories.map(category => <CategoryMenuItem key={category.name} category={category} />)}
