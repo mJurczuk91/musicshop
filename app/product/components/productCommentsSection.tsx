c86ff7af11a8e0f60bce2f5ab60e01bae7ca5654
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-01-15 15:23:31 +0100

    comments section title align fixed (?)

--- app/product/components/productCommentsSection.tsx
+++ app/product/components/productCommentsSection.tsx
@@ -10,8 +10,8 @@ export default async function ProductCommentSection({ productId }: Props) {
     return (
 
         <div className="w-full mb-4 flex flex-col items-center">
-            <div className=" w-full max-w-6xl px-4">
-                <span className=" text-left text-2xl font-bold">Comments</span>
+            <div className="w-full max-w-6xl mx-4 px-4 lg:px-0">
+                <span className="text-2xl font-bold ">Comments</span>
             </div>
             {comments.map(c => {
                 return <CommentDisplay key={c.id} comment={c} abbreviateCommentsLongerThan={150} />
