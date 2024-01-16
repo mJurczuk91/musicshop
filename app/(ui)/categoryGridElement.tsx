56df979f071eddf2cb0eda1a491d6495160d1097
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-01-16 15:58:17 +0100

    category grid fix

--- app/(ui)/categoryGridElement.tsx
+++ app/(ui)/categoryGridElement.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 
 export default function CategoryGridElement({name, subcategories} : {name:string, subcategories:string[]}){
-    return <div style={{}} className="flex justify-between p-2 border-gray-400 border-opacity-25">
-        <div className="flex flex-col">
+    return <div style={{}} className="flex justify-between p-2 border-gray-400 border-opacity-25 odd:text-right md:odd:text-left">
+        <div className="flex flex-col w-full">
             <Link href={`/category/${name}`} className="capitalize font-bold tracking-tight">{name}</Link>
             {subcategories.map(item => {
                 return <Link key={item} className="tracking-tight capitalize" href={`/category/${name}/${item}`}>{item}</Link>
