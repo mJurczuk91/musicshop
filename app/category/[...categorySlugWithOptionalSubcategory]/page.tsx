d9e82e53ebba3d10964ce2dc00b6f7cb342cae43
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-02-07 15:39:58 +0100

    category product grid stub

--- app/category/[...categorySlugWithOptionalSubcategory]/page.tsx
+++ app/category/[...categorySlugWithOptionalSubcategory]/page.tsx
@@ -2,22 +2,26 @@ import { parseSlug } from "@/app/(lib)/helpers";
 import { categories } from "@/app/(lib)/services/categories";
 import { products } from "@/app/(lib)/services/products";
 import CategoryNavigation from "./components/categoryNavigation/categoryNavigation";
+import SortablePaginatedResponsiveProductGrid from "./components/sortablePaginatedResponsiveProductGrid/sortablePaginatedResponsiveProductGrid";
 
 type Props = {
     params: {
         categorySlugWithOptionalSubcategory: [
             'categorySlug',
-            'subcategorySlug'
+            'subcategorySlug',
+            'pageNo'
         ]
     }
 }
 
 export default async function Page({ params }: Props) {
-    const [categorySlug, subcategorySlug] = [...params.categorySlugWithOptionalSubcategory]
-    const paginatedProducts = subcategorySlug ? await products.getBySubcategory(parseSlug(subcategorySlug).id) : await products.getByCategory(parseSlug(categorySlug).id);
+    const [categorySlug, subcategorySlug, pageNo] = [...params.categorySlugWithOptionalSubcategory]
     const allCategories = (await categories.getAll()).data;
 
     return (
-        <CategoryNavigation categoriesJSONstring={JSON.stringify(allCategories)} />
+        <div>
+            <CategoryNavigation categoriesJSONstring={JSON.stringify(allCategories)} />
+            <SortablePaginatedResponsiveProductGrid categorySlug={categorySlug} subcategorySlug={subcategorySlug} pageNo={pageNo} />
+        </div>
     )
 }
