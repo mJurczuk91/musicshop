0626f7abd243d01e4e8092a02a1caf952860979a
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-03-01 23:54:30 +0100

    fix: fixed product miniature styling and link coverage, categorymenu typo

--- app/(ui)/navbar/categoryMenuItem.tsx
+++ app/(ui)/navbar/categoryMenuItem.tsx
@@ -6,7 +6,7 @@ type Props = {
     category: Category,
 }
 
-export default function CategoryMenuItem({category}:Props) {
+export default function CategoryMenuItem({ category }: Props) {
     return <div className="group">
         <Link className="mx-4 capitalize text-white border-tangerine-500 group-hover:border-b-2" href={`/category/${getCategorySlug(category.name, category.id)}`}>
             {category.name}
@@ -14,13 +14,13 @@ export default function CategoryMenuItem({category}:Props) {
         <div className="invisible absolute flex flex-col p-6 bg-darkcyan-500 shadow-md group-hover:visible">
             {category.subcategories.map((subcategory) => {
                 return (
-                <Link 
-                key={subcategory.id}
-                href={`/category/${getSubcategorySlug(subcategory.name, subcategory.id)}`}
-                className="border-b-2 border-darkcyan-500 hover:border-tangerine-500"
-                >
-                    <span className="capitalize">{subcategory.name}</span>
-                </Link>)
+                    <Link
+                        key={subcategory.id}
+                        href={`/category/${getSubcategorySlug(subcategory.name, subcategory.id)}`}
+                        className="border-b-2 border-darkcyan-500 hover:border-tangerine-500"
+                    >
+                        <span className="capitalize">{subcategory.name}</span>
+                    </Link>)
             })}
         </div>
     </div>
