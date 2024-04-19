c630f2110b8cb64ef71a4f93e4a04547cf0e5959
mJurczuk91 <mjurczuk91@gmail.com>
2024-04-19 14:36:44 +0200

    fix: more main page spacing fixes

--- app/components/blogArticleStubs.tsx
+++ app/components/blogArticleStubs.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default async function BlogArticleStubs() {
     const stubs = (await articles.getPage()).data;
-    return <div className="flex mt-4 py-8 w-full justify-center border-t-2 border-b-2 border-t-gray-400 border-b-gray-400 border-opacity-25">
+    return <div className="flex py-8 w-full justify-center border-t-gray-400 border-b-gray-400 border-opacity-25">
         <div className="max-w-6xl flex flex-col">
             {stubs.map((stub, index) => <ArticleStub key={index} stub={stub} />)}
         </div>
@@ -12,7 +12,7 @@ export default async function BlogArticleStubs() {
 }
 
 function ArticleStub({ stub }: { stub: BlogArticleStub }) {
-    return <div className="mx-4 mb-2 p-2 basis-1/3 bg-white shadow-lg border-darkcyan-100 border w-full flex-col lg:flex md:odd:flex-row-reverse">
+    return <div className="my-4 basis-1/3 bg-white border-darkcyan-100 border w-full flex lg:odd:flex-row-reverse">
         <div className="flex justify-center p-8 basis-1/2 w-full">
             <img className="w-64" src={stub.image_url} alt="article image"></img>
         </div>
--- app/components/productMiniature.tsx
+++ app/components/productMiniature.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function ProductMiniature({ product }: Props) {
     const description_short = product.description.slice(0, 30).trim();
     return <Link href={`/product/${getProductSlug(product)}`}>
-        <div className="bg-white p-4 m-4 h-96 border-darkcyan-100 border hover:shadow-xl hover:border-darkcyan-500 flex flex-col justify-between">
+        <div className="bg-white p-4 my-4 h-96 border-darkcyan-100 border hover:shadow-xl hover:border-darkcyan-500 flex flex-col justify-between">
             <div className="text-center flex flex-col basis-2/3 justify-between ">
                 <img
                     className="h-auto w-[16rem] max-w-full"
