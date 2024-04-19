6327d5089f0f3302f1e141b94f13c276834afc27
mJurczuk91 <mjurczuk91@gmail.com>
2024-04-19 12:53:41 +0200

    fix: searchbar debounce now works properly

--- app/components/navbar/searchbar/searchbar.tsx
+++ app/components/navbar/searchbar/searchbar.tsx
@@ -1,77 +1,76 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
 import SearchResult from "./searchResult";
 import { Product } from "@/app/(lib)/definitions";
 import SearchResultLoading from "./searchResultLoading";
 
-let timer: NodeJS.Timeout | null = null
-
 export default function Searchbar() {
-    const [query, setQuery] = useState<string>('');
-    const [data, setData] = useState<Product[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [focus, setFocus] = useState<boolean>(false);
-    const [searchFieldEmpty, setSearchFieldEmpty] = useState<boolean>(true);
+  const [query, setQuery] = useState<string>("");
+  const [data, setData] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [focus, setFocus] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (query.length === 0) return;
-        fetch(`/api/search?query=${query}`)
-            .then(response => response.json())
-            .then(data => {
-                setData(data);
-            })
-            .finally(
-                () => {
-                    setTimeout(() => setLoading(false), 200);
-                }
-            );
-        return () => {
-            if (timer) {
-                clearTimeout(timer);
-            }
-        }
-    }, [query]);
+  useEffect(() => {
+    if (query.length === 0) return;
+    setLoading(true);
+    const timer = setTimeout(() => {
+      fetch(`/api/search?query=${query}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }, 300);
 
-    return <div className="w-full max-w-sm">
-        <input 
-            autoComplete='off'
-            className="p-2 w-full text-darkcyan-900 border-tangerine-500 bg-white border-2 rounded-md focus:outline-none"
-            type="text" placeholder="Search our catalogue" name="searchField"
-            onFocus={() => {
-                setFocus(true);
-            }}
-            onBlur={() => {
-                setTimeout(() => {
-                    setFocus(false);
-                }, 100)
-            }}
-            onChange={
-                (e: React.ChangeEvent<HTMLInputElement>) => {
-                    const value = e.currentTarget.value;
-                    if(value.length === 0){
-                        setSearchFieldEmpty(true);
-                    } else {
-                        setLoading(true);
-                        setSearchFieldEmpty(false)
-                    }
-                    if (timer) {
-                        clearTimeout(timer);
-                    }
-                    timer = setTimeout(() => {
-                        setQuery(value);
-                    }, 500)
-                }} />
-            <div className={`absolute w-full max-w-sm flex flex-col ${focus && !searchFieldEmpty ? 'visible' : 'invisible'}`}>
-                {loading ? 
-                <div className="w-full flex items-center">
-                    <SearchResultLoading /> 
-                </div>
-                :
-                data.length === 0 ?
-                    <SearchResult />
-                    :
-                    data.map(product => <SearchResult key={product.id} product={product} />)}
-            </div>
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [query]);
+
+  return (
+    <div className="w-full max-w-sm">
+      <input
+        autoComplete="off"
+        className="p-2 w-full text-darkcyan-900 border-tangerine-500 bg-white border-2 rounded-md focus:outline-none"
+        type="text"
+        placeholder="Search our catalogue"
+        name="searchField"
+        onFocus={() => {
+          setFocus(true);
+        }}
+        onBlur={() => {
+          setTimeout(() => {
+            setFocus(false);
+          }, 100);
+        }}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          const value = e.currentTarget.value;
+
+          setQuery(value);
+        }}
+      />
+      <div
+        className={`absolute w-full max-w-sm flex flex-col ${
+          focus && query ? "visible" : "invisible"
+        }`}
+      >
+        {loading ? (
+          <div className="w-full flex items-center">
+            <SearchResultLoading />
+          </div>
+        ) : data.length === 0 ? (
+          <SearchResult />
+        ) : (
+          data.map((product) => (
+            <SearchResult key={product.id} product={product} />
+          ))
+        )}
+      </div>
     </div>
+  );
 }
